b6e69edf7799aa6e246ea5ab8096208c2c26be36
ArqdsisPT <git@github.com>
2016-03-16 15:23:02 -0300

    Aula2

---
+++ src/TestaAlunos.java
@@ -0,0 +1,17 @@
+public class TestaAlunos {
+
+	public static void main(String[] args) {
+
+    AlunosCRUD alunos = new AlunosCRUD("Paulo","rg","end","email",7887878,8766576);
+	alunos.cadastrar();
+	alunos.consultar();
+	System.out.println(alunos);
+	alunos.setNome("Thais");
+	alunos.alterar();
+	alunos.consultar();
+	System.out.println(alunos);
+	alunos.deletar();
+	
+	}
+
+}
---
+++ src/TestaCursos.java
@@ -0,0 +1,19 @@
+
+public class TestaCursos {
+
+	public static void main(String[] args) {
+
+		CursosCRUD cursos = new CursosCRUD("cod", "Informatica", "numLab", "regSoft", "material", "livro", "dataIni",
+				"dataTer", "horario", 999, 2500.00);
+		cursos.cadastrar();
+		cursos.consultar();
+		System.out.println(cursos);
+		cursos.setNome("Matematica");
+		cursos.alterar();
+		cursos.consultar();
+		System.out.println(cursos);
+		cursos.deletar();
+
+	}
+
+}
